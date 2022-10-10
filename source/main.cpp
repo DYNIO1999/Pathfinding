7ca4d7d6dcee910ede7bc73d6e28f747deb2cb91
Daniel Skrobot <dd7412506@gmail.com>
2022-10-10 22:21:40 +0200

    setup

---
+++ source/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <vulkan/vulkan.h>
+
+
+int main()
+{
+
+
+    return EXIT_SUCCESS;
+}
---
+++ unixRun.sh
@@ -0,0 +1,3 @@
+cd build
+./VulkanPathfinding/VulkanPathfinding
+cd ..
